090cb257b4ec6826ca8ba16e14f601dd31afe819
caijx <caijx@ggxx.com>
2022-04-14 15:53:34 +0800

    test

---
+++ src/Hello.php
@@ -0,0 +1,9 @@
+<?php
+namespace choijx\NowipaySdk;
+
+class Hello {
+    public function say()
+    {
+        echo 'hello';
+    }
+}
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use choijx\NowipaySdk\Hello;
+
+$hello = new Hello();
+$hello->say();
